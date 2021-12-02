2e21b5e7748cc6393588909f5de7369292ceec10
Russ Zeien <rczeien@gmail.com>
2021-12-02 10:52:06 -0500

    huh, python has a match keyword now

--- 2021/day2/part2.py
+++ 2021/day2/part2.py
@@ -5,13 +5,14 @@ with open('input', 'r') as data:
         command = cur.strip().split(' ')
         mod = int(command[1])
 
-        if command[0] == 'up':
-            aim -= mod
-        elif command[0] == 'down':
-            aim += mod
-        elif command[0] == 'forward':
-            x += mod
-            y += aim * mod
-
+        match command[0]:
+            case 'up':
+                aim -= mod
+            case 'down':
+                aim += mod
+            case 'forward':
+                x += mod
+                y += aim * mod
+        
 print(f'{x}, {y}')
 print(x * y)
