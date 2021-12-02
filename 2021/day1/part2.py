e071e3d4b03ad1da804512c7499120bbca7cd0d0
Russ Zeien <rczeien@gmail.com>
2021-12-02 08:30:40 -0500

    window size

--- 2021/day1/part2.py
+++ 2021/day1/part2.py
@@ -1,3 +1,4 @@
+window = 3
 num_incr = -1
 cur = 0
 prev = -1
@@ -5,8 +6,8 @@ prev = -1
 with open('input', 'r') as data:
     all_values = [int(val) for val in data.readlines()]
 
-for i in range(len(all_values) - 2):
-    cur = sum(all_values[i:i+3])
+for i in range(len(all_values) - (window - 1)):
+    cur = sum(all_values[i:i+window])
     if cur > prev:
         num_incr += 1
 
